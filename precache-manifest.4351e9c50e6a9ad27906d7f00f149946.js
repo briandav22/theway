b4427f31ad50781dd225d71886878aa24e517ac3
Brian Davenport <briandav22@gmail.com>
2019-11-21 17:03:42 -0500

    Updates

---
+++ precache-manifest.4351e9c50e6a9ad27906d7f00f149946.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55e65ebe63aed4881ed4d616dc76b8bf",
+    "url": "/theway/index.html"
+  },
+  {
+    "revision": "a01da5c0a9ca350f00c6",
+    "url": "/theway/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "31c38820f65ccee92a00",
+    "url": "/theway/static/css/main.1cfa4af1.chunk.css"
+  },
+  {
+    "revision": "a01da5c0a9ca350f00c6",
+    "url": "/theway/static/js/2.df7be6a3.chunk.js"
+  },
+  {
+    "revision": "31c38820f65ccee92a00",
+    "url": "/theway/static/js/main.52a794ef.chunk.js"
+  },
+  {
+    "revision": "416f9f7454e1570872bd",
+    "url": "/theway/static/js/runtime-main.c25d8c9d.js"
+  },
+  {
+    "revision": "93bc056817a61450c55b733ba7bea075",
+    "url": "/theway/static/media/aboutus.93bc0568.jpg"
+  },
+  {
+    "revision": "ba918e322391777810ff12d2a6095a24",
+    "url": "/theway/static/media/background.ba918e32.jpg"
+  }
+]);
